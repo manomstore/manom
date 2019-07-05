3a7d70f61000c4085ef01a76179aa2de8122f1cb
Volosojui <alessandro.kalinin@gmail.com>
2019-07-05 13:38:16 +0300

    Фикс кэша стилей

--- local/templates/manom/header.php
+++ local/templates/manom/header.php
@@ -34,7 +34,7 @@
 	<link rel="stylesheet" href="<?= SITE_TEMPLATE_PATH ?>/assets/css/main.min-2.css">
 
 	<link rel="stylesheet" href="<?= SITE_TEMPLATE_PATH ?>/assets/js/coffee/pushUpJS/pushUp.css">
-	<link rel="stylesheet" href="<?= SITE_TEMPLATE_PATH ?>/assets/css/custom.css">
+	<link rel="stylesheet" href="<?= SITE_TEMPLATE_PATH ?>/assets/css/custom.css?v=2">
 	<link rel="stylesheet"
 	      href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
 	      integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
