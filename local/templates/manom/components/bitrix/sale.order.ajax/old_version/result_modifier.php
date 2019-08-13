0bf50da69749b5021aeab884a69d56e1c259171f
Pavel <tech@hozberg.com>
2019-08-13 16:54:08 +0300

    Свойства заказа только для Москвы

--- local/templates/manom/components/bitrix/sale.order.ajax/old_version/result_modifier.php
+++ local/templates/manom/components/bitrix/sale.order.ajax/old_version/result_modifier.php
@@ -6,3 +6,23 @@ if (!$USER->IsAuthorized()) {
         $delivery["CHECKED"] = "N";
     }
 }
+
+$isMoscowLocations = false;
+
+foreach ($arResult["ORDER_PROP"]["USER_PROPS_Y"] as $property) {
+    if ($property["CODE"] === "LOCATION") {
+        $isMoscowLocations = (int)$property["VALUE"] === 84;
+        break;
+    }
+}
+
+if (!$isMoscowLocations) {
+    foreach ($arResult["ORDER_PROP"]["USER_PROPS_Y"] as &$property) {
+        if (in_array($property["CODE"], ["DATE_DELIVERY", "TIME_DELIVERY"])) {
+            $property = null;
+        }
+    }
+    $arResult["ORDER_PROP"]["USER_PROPS_Y"] = array_filter($arResult["ORDER_PROP"]["USER_PROPS_Y"]);
+
+    unset($property);
+}
