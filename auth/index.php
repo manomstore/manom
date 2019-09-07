22ed5797da8045f96fe7d56794e185ca7f73aac9
Pavel <tech@hozberg.com>
2019-09-07 04:46:19 +0300

    Авторегистрация

--- auth/index.php
+++ auth/index.php
@@ -1,5 +1,7 @@
 <?
-//define("NEED_AUTH", true);
+if ($_REQUEST["change_password"] === "yes") {
+    define("NEED_AUTH", true);
+}
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 //if(isset($_REQUEST['backurl']) && strlen($_REQUEST['backurl'])>0)
 //    LocalRedirect($backurl);
