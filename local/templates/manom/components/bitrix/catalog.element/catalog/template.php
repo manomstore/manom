f161266410f7cd7fcf4ec512bb8473d8270f8ce2
Pavel <pavel@hozberg.com>
2019-09-25 14:13:59 +0300

    Отзывы

--- local/templates/manom/components/bitrix/catalog.element/catalog/template.php
+++ local/templates/manom/components/bitrix/catalog.element/catalog/template.php
@@ -664,6 +664,8 @@ if (!$_REQUEST['offer']){
                                 <?= !empty($value["source"]) ? $value["source"] . "." : "" ?>
                             </span>
                             <span class="tab-content__text1"><?= $value['review_text'] ?></span>
+                            <span class="tab-content__text1">Достоинства: <?= $value['merits'] ?></span>
+                            <span class="tab-content__text1">Недостатки: <?= $value['disadvantages'] ?></span>
                         </p>
                     <? } ?>
 
