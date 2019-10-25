3d8b605b1be3cb7e1a6701356e27f49d89fcd5d7
igorek-dev <56792240+igorek-dev@users.noreply.github.com>
2019-10-25 15:30:06 +0300

    update_cart_template
    Добавил отображение модели товара ( Если оно есть ) в корзине.

--- local/templates/manom/components/bitrix/sale.basket.basket/manom/template.php
+++ local/templates/manom/components/bitrix/sale.basket.basket/manom/template.php
@@ -22,9 +22,19 @@ use Bitrix\Main\Localization\Loc;
 <? if (count($arResult['GRID']['ROWS']) <= 0): ?>
     <script type="text/javascript">
         location.href = '/';
+/*Выполняю запрос к таблице b_iblock_element_property для выборки данных о дополнительных характеристиках товара*/
+$arr=$arResult['BASKET_ITEM_RENDER_DATA'][$i]['PRODUCT_ID'];$results=$DB->Query("SELECT * FROM `b_iblock_element_property` WHERE `IBLOCK_ELEMENT_ID`='$arr'"); while ($raw=$results->Fetch()) {$res=$DB->Query("SELECT * FROM `b_iblock_property` WHERE `ID`='$raw[IBLOCK_PROPERTY_ID]'"); $ro=$res->Fetch(); if($ro['NAME']=='Модель'){echo 'Модель : '.$raw['VALUE'];}else{}}?>
     </script>
 <? endif ?>
-<? foreach ($arResult['GRID']['ROWS'] as $key => $row): ?>
+<? 
+	#foreach ($arResult['GRID']['ROWS'] as $key => $row): 
+#Заменил цикл foreach на цикл for
+for($i=0;$i<count($arResult['GRID']['ROWS']);$i++) : 
+#Получаю ID товаров добавленных в корзину
+$ids=$arResult['BASKET_ITEM_RENDER_DATA'][$i]['ID'];
+$row=$arResult['GRID']['ROWS'][$ids]; 
+
+?>
     <article class="sci-product<?= !$row['has_prod'] ? ' sci-product--off' : ''; ?>"
              data-id="<?= $row['ID'] ?>">
         <div class="sci-product__wrapper">
@@ -58,7 +68,29 @@ use Bitrix\Main\Localization\Loc;
                     <h3 class="sci-product__name">
                         <?= $row['NAME'] ?>
                     </h3>
-                </a>
+		    </a>
+		    <?
+	/*Выполняю запрос к таблице b_iblock_element_property для выборки данных о дополнительных характеристиках товара*/
+$arr=$arResult['BASKET_ITEM_RENDER_DATA'][$i]['PRODUCT_ID'];
+			    $results=$DB->Query("SELECT * FROM `b_iblock_element_property` WHERE `IBLOCK_ELEMENT_ID`='$arr'"); 
+			    while ($raw=$results->Fetch())
+			    {
+			    /*Выполняю запрос к таблице b_iblock_property для выборки значения характеристик*/
+
+				$res=$DB->Query("SELECT * FROM `b_iblock_property` WHERE `ID`='$raw[IBLOCK_PROPERTY_ID]'"); 
+				    $ro=$res->Fetch(); 
+			/*Если значение названия характеристики == Модель то вывожу знаечение*/
+
+				    if($ro['NAME']=='Модель')
+				    {
+					    echo 'Модель : '.$raw['VALUE'];
+				    }
+				    else
+				    {
+				    }
+			    }
+		    ?>
+
                 <p class="sci-product__status">
                     <? if ($row['has_prod']) { ?>
                         Есть в наличии
@@ -183,7 +215,7 @@ use Bitrix\Main\Localization\Loc;
             </div>
         <? endif; ?>
     </article>
-<? endforeach; ?>
+<? endfor; ?>
 <? if (!empty($arResult['GRID']['ROWS'])): ?>
     <button class="button-del button-del--bottom js-basket-clear" type="button">Очистить корзину</button>
 <? endif; ?>
