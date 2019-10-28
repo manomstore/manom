e8266809bc8d8dfed5ccd1a1e8eb585885d2d094
Volosojui <alessandro.kalinin@gmail.com>
2019-10-28 13:37:39 +0300

    Подправил стили плашки про бесплатную доставку

--- local/templates/manom/header.php
+++ local/templates/manom/header.php
@@ -253,11 +253,11 @@ $userCityByGeoIP = $userLocationInfo;
 					false
 				); ?>
 
-				<a href="" class="top-ad-line__close">
-					<img src="<?= SITE_TEMPLATE_PATH ?>/assets/img/top-close.svg" alt="">
-				</a>
 			</div>
 		</div>
+		<a href="#" class="top-ad-line__close">
+			<svg xmlns="http://www.w3.org/2000/svg" version="1" viewBox="0 0 24 24"><path d="M13 12l5-5-1-1-5 5-5-5-1 1 5 5-5 5 1 1 5-5 5 5 1-1z"></path></svg>
+		</a>
 	</div>
 
     <div class="header__wrapper">
