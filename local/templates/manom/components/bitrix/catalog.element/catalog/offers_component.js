4272d8c9c34fddad83c71f21b6d37c546b0fad4b
Volosojui <alessandro.kalinin@gmail.com>
2019-12-06 11:11:02 +0300

    console.log

--- local/templates/manom/components/bitrix/catalog.element/catalog/offers_component.js
+++ local/templates/manom/components/bitrix/catalog.element/catalog/offers_component.js
@@ -18,6 +18,10 @@ BX.namespace('BX');
             this.currentOfferId = Number(parameters.currentOfferId)
             this.offerProperties = parameters.offerProperties
 
+            console.log('available_Offers'.toUpperCase() + ': ', this.availableOffers);
+            console.log('current_Offer_Id'.toUpperCase() + ': ', this.currentOfferId);
+            console.log('offer_Properties'.toUpperCase() + ': ', this.offerProperties);
+
         },
 
         render: function () {
