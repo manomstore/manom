c5bde51bf78a8a0c8cf78a4dd4a54431189c52d6
Ksenia Balashova <utyatka@gmail.com>
2020-02-17 11:18:57 +0100

    separator fix

--- local/templates/manom/components/bitrix/breadcrumb/articles/template.php
+++ local/templates/manom/components/bitrix/breadcrumb/articles/template.php
@@ -27,12 +27,11 @@ $itemSize = count($arResult);
 for($index = 0; $index < $itemSize; $index++)
 {
 	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
-	$arrow = ($index > 0? '<span class="bread-crumb__separator"> <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"     width="10px"
-	height="6px" x="10px" y="6px" fill-opacity="0.2"
-	viewBox="0 0 10 10" style="enable-background:new 0 0 10 10;" xml:space="preserve">
-<polygon points="112.814,0 91.566,21.178 273.512,203.718 91.566,386.258 112.814,407.436 315.869,203.718 "/>
-</svg>
-</span>' : '');
+	$arrow = ($index > 0? '<span class="bread-crumb__separator">
+		<svg version="1.1" width="10px" height="6px" x="10px" y="6px" fill-opacity="0.2" viewBox="0 0 10 10">
+			<polygon points="112.814,0 91.566,21.178 273.512,203.718 91.566,386.258 112.814,407.436 315.869,203.718 "/>
+		</svg>
+	</span>' : '');
 
 	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
 	{
