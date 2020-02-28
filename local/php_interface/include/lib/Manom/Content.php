e3322c346a4f49a2474f631ac7a31ca6b3016c7e
Aleksei Litvishko <uthhar@yandex.ru>
2020-02-28 17:48:30 +0700

    карточка товара

--- local/php_interface/include/lib/Manom/Content.php
+++ local/php_interface/include/lib/Manom/Content.php
@@ -19,6 +19,36 @@ class Content
     }
 
     /**
+     * Метод возвращает окончание для множественного числа слова на основании числа и массива окончаний
+     * @param int $iNumber - Число на основе которого нужно сформировать окончание
+     * @param array $aEnding - Массив слов или окончаний для чисел (1, 4, 5), например array("яблоко", "яблока", "яблок")
+     * @return string
+     */
+    public static function getNumEnding(int $iNumber, array $aEnding): string
+    {
+        $iNumber = $iNumber % 100;
+        if ($iNumber >= 11 && $iNumber <= 19) {
+            $sEnding = $aEnding[2];
+        } else {
+            $i = $iNumber % 10;
+            switch ($i) {
+                case (1):
+                    $sEnding = $aEnding[0];
+                    break;
+                case (2):
+                case (3):
+                case (4):
+                    $sEnding = $aEnding[1];
+                    break;
+                default:
+                    $sEnding = $aEnding[2];
+            }
+        }
+
+        return $sEnding;
+    }
+
+    /**
      * @param int $sectionId
      * @return array
      * @throws Exception
--- local/templates/manom/components/bitrix/catalog.section/recom/template.php
+++ local/templates/manom/components/bitrix/catalog.section/recom/template.php
@@ -1,118 +1,104 @@
-<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+<?php
 
-use \Bitrix\Main\Localization\Loc;
-
-/**
- * @global CMain $APPLICATION
- * @var array $arParams
- * @var array $arResult
- * @var CatalogSectionComponent $component
- * @var CBitrixComponentTemplate $this
- * @var string $templateName
- * @var string $componentPath
- */
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
 
 $this->setFrameMode(true);
-// echo "<pre style='text-align:left;'>";print_r($arResult['REVIEW']);echo "</pre>";
 ?>
-<?if ($arResult['ITEMS']):?>
-<!-- Дополнительное предложение -->
-<section class="additionally">
-	<div class="container">
-		<h2 class="additionally-h2">Вас может заинтересовать</h2>
-		<div class="additionally__block row">
-		<?foreach ($arResult['ITEMS'] as $key => $value) {?>
-			<?$arCanBuy = CCatalogSKU::IsExistOffers($value['ID'], $arResult['IBLOCK_ID']);?>
-			<div class="col-3">
-				<div class="product-card border">
-					<div class="product-card__img">
-						<?foreach ($value['PROPERTIES']['MORE_PHOTO']['VALUE'] as $p => $photo) {?>
-							<div class="product-card__slide">
-								<img src="<?=CFile::GetPath($photo);?>" alt="">
-							</div>
-						<?}?>
-					</div>
-					<p class="p-label-top active">
-							<?if ($value['PROPERTIES']['PRODUCT_OF_THE_DAY']['VALUE'] == 'Да') {?>
-							Товар дня
-							<?}?>
-						</p>
-					<div class="p-nav-top">
-						<label>
-							<input class="p-nav-top__checkbox" type="checkbox">
-							<div class="p-nav-top__favorite" title="в избранное"></div>
-						</label>
-						<div class="p-nav-top__list"></div>
-					</div>
-					<div class="p-nav-middle">
-						<?if ($value['PROPERTIES']['SELL_PROD']['VALUE'] == 'Да') {?>
-						<div class="p-nav-middle__sale active">Распродажа</div>
-						<?}?>
+<?php if ($arResult['ITEMS']): ?>
+    <section class="additionally">
+        <div class="container">
+            <h2 class="additionally-h2">Вас может заинтересовать</h2>
+            <div class="additionally__block row">
+                <?php foreach ($arResult['ITEMS'] as $item): ?>
+                    <?php
+                    $class1 = $item['inFavoriteAndCompare'] ? '' : 'notActive';
+                    $class2 = $item['inFavoriteAndCompare'] ? 'alt-img' : 'notActive';
 
-<!--						<div class="p-nav-middle__rating">-->
-<!--							--><?//for ($i=0; $i < 5; $i++) {
-//								if ($i >= $arResult['REVIEW'][$value['ID']]['rating']) {
-//									?><!-- <span> ★ </span> --><?//
-//								}else{
-//									?><!-- ★ --><?//
-//								}
-//							}?><!--</div>-->
-<!--						<div class="p-nav-middle__comments"><span>--><?//=$arResult['REVIEW'][$value['ID']]['count']?><!--</span></div>-->
-					</div>
-					<h3 class="p-name">
-						<a href="<?=$value['DETAIL_PAGE_URL']?>"><?=$value['NAME']?></a>
-					</h3>
-					<div class="p-nav-bottom">
-						<?
-						$arPrice = CCatalogProduct::GetOptimalPrice($value['ID'], 1, $USER->GetUserGroupArray());
-						?>
-						<div class="p-nav-bottom__price">
-							<?=$arPrice['RESULT_PRICE']['DISCOUNT_PRICE']?><span> ₽</span>
-							<?if($arPrice['RESULT_PRICE']['BASE_PRICE'] != $arPrice['RESULT_PRICE']['DISCOUNT_PRICE']){?>
-								<div class="p-nav-bottom__oldprice"><?=$arPrice['RESULT_PRICE']['BASE_PRICE']?></div>
-							<?}?>
-						</div>
-						<div class="p-nav-bottom__shopcart <?=$value['CATALOG_QUANTITY'] > 0 || $arCanBuy == 1 ? 'addToCartBtn' : ''?>" data-id='<?=$arPrice['PRODUCT_ID']?>' <?=$value['CATALOG_QUANTITY'] > 0 || $arCanBuy == 1 ? 'enable' : 'disable'?>></div>
-					</div>
-				</div>
-			</div>
-		<?}?>
-		<!-- <div class="col-3">
-			<div class="product-card border">
-				<div class="product-card__img">
-					<div class="product-card__slide">
-						<img src="img/4396804.jpg" alt="">
-					</div>
-					<div class="product-card__slide">
-						<img src="img/4396801.jpg" alt="">
-					</div>
-				</div>
-				<p class="p-label-top">Товар дня</p>
-				<div class="p-nav-top">
-					<label>
-						<input class="p-nav-top__checkbox" type="checkbox">
-						<div class="p-nav-top__favorite" title="в избранное"></div>
-					</label>
-					<div class="p-nav-top__list"></div>
-				</div>
-				<div class="p-nav-middle">
-					<div class="p-nav-middle__sale active">Распродажа</div>
-					<div class="p-nav-middle__rating">★ ★ ★ <span>★ ★</span></div>
-					<div class="p-nav-middle__comments"><span>12</span></div>
-				</div>
-				<h3 class="p-name">
-					Смартфон Apple iPhone SE 32Gb Space Gray MP822RU/A, серый Смартфон Apple
-				</h3>
-				<div class="p-nav-bottom">
-					<div class="p-nav-bottom__price">
-						5 000<span> ₽</span>
-						<div class="p-nav-bottom__oldprice">40 000</div>
-					</div>
-					<div class="p-nav-bottom__shopcart"></div>
-				</div>
-			</div>
-		</div> -->
-		</div>
-	</div>
-</section>
-<?endif;?>
+                    [$price, $oldPrice] = $item['price']['PRICES'];
+                    ?>
+                    <div class="col-3">
+                        <div class="product-card border">
+                            <div class="product-card__img">
+                                <?php foreach ($item['images'] as $image): ?>
+                                    <div class="product-card__slide">
+                                        <img
+                                                src="<?=$image['src']?>"
+                                                alt="<?=$item['name']?>"
+                                        >
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
+                            <p class="p-label-top active">
+                                <?php if ($item['productOfTheDay']): ?>
+                                    Товар дня
+                                <?php endif; ?>
+                            </p>
+                            <div class="p-nav-top">
+                                <label>
+                                    <input
+                                            class="p-nav-top__checkbox"
+                                            type="checkbox"
+                                        <?=$item['inFavoriteAndCompare'] ? 'checked' : ''?>
+                                    >
+                                    <div
+                                            class="p-nav-top__favorite addToFavoriteList <?=$class1?>"
+                                            data-id='<?=$item['id']?>'
+                                            title="в избранное"
+                                    ></div>
+                                </label>
+                                <div
+                                        class="p-nav-top__list addToCompareList <?=$class2?>"
+                                        data-id='<?=$item['id']?>'
+                                ></div>
+                            </div>
+                            <div class="p-nav-middle">
+                                <?php if ($item['sale']): ?>
+                                    <div class="p-nav-middle__sale active">Распродажа</div>
+                                <?php endif; ?>
+
+                                <?php /*
+                                <div class="p-nav-middle__rating">
+                                    <?php for ($i = 0; $i < 5; $i++): ?>
+                                        <?php if ($i >= $item['rating']['rating']): ?>
+                                            <span> ★</span>
+                                        <?php else: ?>
+                                            ★
+                                        <?php endif; ?>
+                                    <?php endfor; ?>
+                                </div>
+                                <div class="p-nav-middle__comments">
+                                    <span><?=$item['rating']['count']?></span>
+                                </div>
+                                */ ?>
+                            </div>
+                            <h3 class="p-name">
+                                <a href="<?=$item['url']?>"><?=$item['name']?></a>
+                            </h3>
+                            <div class="p-nav-bottom">
+                                <div class="p-nav-bottom__price">
+                                    <?=number_format($price, 0, '', ' ')?>
+                                    <span> ₽</span>
+                                    <?php if (
+                                        !empty((int)$oldPrice) &&
+                                        (int)$price !== (int)$oldPrice
+                                    ): ?>
+                                        <div class="p-nav-bottom__oldprice">
+                                            <?=number_format($oldPrice, 0, '', ' ')?>
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
+                                <div
+                                        class="p-nav-bottom__shopcart <?=$item['canBuy'] ? 'addToCartBtn' : ''?>"
+                                        data-id='<?=$item['productId']?>'
+                                    <?=$item['canBuy'] ? 'enable' : 'disable'?>
+                                ></div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </section>
+<?php endif; ?>
