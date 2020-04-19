55ba5e7c7a81fcc4231059164eec4e55b5626748
Ksenia Balashova <utyatka@gmail.com>
2020-04-19 20:08:37 +0200

    add ui

--- local/templates/manom/assets/js/ui-accord.js
+++ local/templates/manom/assets/js/ui-accord.js
@@ -1,34 +1,36 @@
 'use strict';
-
-var $uiAccordion = $('.js-ui-accordion');
-
-$uiAccordion.accordion({
-  collapsible: true,
-  heightStyle: 'content',
-
-  activate: function activate(event, ui) {
-    var newHeaderId = ui.newHeader.attr('id');
-
-    if (newHeaderId) {
-      history.pushState(null, null, '#' + newHeaderId);
-    }
-  },
-
-  create: function create(event, ui) {
-    var $this = $(event.target);
-    var $activeAccordion = $(window.location.hash);
-
-    if ($this.find($activeAccordion).length) {
-      $this.accordion('option', 'active', $this.find($this.accordion('option', 'header')).index($activeAccordion));
-    }
-  }
+$(".js-ui-accordion").accordion({
+  active: 2
 });
+// var $uiAccordion = $('.js-ui-accordion');
 
-$(window).on('hashchange', function (event) {
-  var $activeAccordion = $(window.location.hash);
-  var $parentAccordion = $activeAccordion.parents('.js-ui-accordion');
+// $uiAccordion.accordion({
+//   collapsible: true,
+//   heightStyle: 'content',
 
-  if ($activeAccordion.length) {
-    $parentAccordion.accordion('option', 'active', $parentAccordion.find($uiAccordion.accordion('option', 'header')).index($activeAccordion));
-  }
-});
+//   activate: function activate(event, ui) {
+//     var newHeaderId = ui.newHeader.attr('id');
+
+//     if (newHeaderId) {
+//       history.pushState(null, null, '#' + newHeaderId);
+//     }
+//   },
+
+//   create: function create(event, ui) {
+//     var $this = $(event.target);
+//     var $activeAccordion = $(window.location.hash);
+
+//     if ($this.find($activeAccordion).length) {
+//       $this.accordion('option', 'active', $this.find($this.accordion('option', 'header')).index($activeAccordion));
+//     }
+//   }
+// });
+
+// $(window).on('hashchange', function (event) {
+//   var $activeAccordion = $(window.location.hash);
+//   var $parentAccordion = $activeAccordion.parents('.js-ui-accordion');
+
+//   if ($activeAccordion.length) {
+//     $parentAccordion.accordion('option', 'active', $parentAccordion.find($uiAccordion.accordion('option', 'header')).index($activeAccordion));
+//   }
+// });
