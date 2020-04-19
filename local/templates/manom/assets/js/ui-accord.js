1114fa49aba2c705a035df6e62fa8880b1199e2d
Ksenia Balashova <utyatka@gmail.com>
2020-04-19 22:04:50 +0200

    wip

--- local/templates/manom/assets/js/ui-accord.js
+++ local/templates/manom/assets/js/ui-accord.js
@@ -1,28 +1,11 @@
 'use strict';
 
-// $(function () {
-//   $("#accordion").accordion({
-//     active: 1,
-//     collapsible: true
-//   });
-
-// });
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.hight === "100%") {
-      panel.style.height = "0";
-    } else {
-      panel.style.height = "100%";
-    }
+$(function () {
+  $("#accordion").accordion({
+    active: 1,
+    collapsible: true
   });
-}
+
+});
+
+
