29d8c6fa4114317f257455332749386e1e8ec70f
Ksenia Balashova <utyatka@gmail.com>
2020-04-19 21:21:16 +0200

    p cover

--- local/templates/manom/assets/js/ui-accord.js
+++ local/templates/manom/assets/js/ui-accord.js
@@ -6,36 +6,3 @@ $(function () {
     active: 1
   });
 })
-
-// var $uiAccordion = $('.js-ui-accordion');
-
-// $uiAccordion.accordion({
-//   collapsible: true,
-//   heightStyle: 'content',
-
-//   activate: function activate(event, ui) {
-//     var newHeaderId = ui.newHeader.attr('id');
-
-//     if (newHeaderId) {
-//       history.pushState(null, null, '#' + newHeaderId);
-//     }
-//   },
-
-//   create: function create(event, ui) {
-//     var $this = $(event.target);
-//     var $activeAccordion = $(window.location.hash);
-
-//     if ($this.find($activeAccordion).length) {
-//       $this.accordion('option', 'active', $this.find($this.accordion('option', 'header')).index($activeAccordion));
-//     }
-//   }
-// });
-
-// $(window).on('hashchange', function (event) {
-//   var $activeAccordion = $(window.location.hash);
-//   var $parentAccordion = $activeAccordion.parents('.js-ui-accordion');
-
-//   if ($activeAccordion.length) {
-//     $parentAccordion.accordion('option', 'active', $parentAccordion.find($uiAccordion.accordion('option', 'header')).index($activeAccordion));
-//   }
-// });
