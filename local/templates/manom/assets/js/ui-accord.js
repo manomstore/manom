42447a8dbee963f0bf5c533e9d88c080433f1b5b
Ksenia Balashova <utyatka@gmail.com>
2020-04-19 21:58:17 +0200

    wip

--- local/templates/manom/assets/js/ui-accord.js
+++ local/templates/manom/assets/js/ui-accord.js
@@ -1,9 +1,28 @@
 'use strict';
 
-$(function () {
-  $("#accordion").accordion({
-    active: 1,
-    collapsible: true
-  });
+// $(function () {
+//   $("#accordion").accordion({
+//     active: 1,
+//     collapsible: true
+//   });
 
-});
+// });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
