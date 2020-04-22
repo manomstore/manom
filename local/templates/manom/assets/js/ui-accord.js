977e4d9cfb0f9abe81099bfd39f769984f09e067
Ksenia Balashova <utyatka@gmail.com>
2020-04-22 19:09:43 +0200

    wip

--- local/templates/manom/assets/js/ui-accord.js
+++ local/templates/manom/assets/js/ui-accord.js
@@ -13,6 +13,18 @@ $(function () {
     });
   }
 
+  function scrollTo(element) {
+    jQuery('html, body').animate({
+      scrollTop: jQuery(element).offset().top
+    }, 500);
+  }
+
+  jQuery('.accordion').on('accordionactivate', function (event, ui) {
+    scrollTo(jQuery(event.target).find('.ui-accordion-header-active'))
+  });
+
 });
+
+
 
 
