8fa1e1645762f53910d54ed798860ea2019c48c3
Ksenia Balashova <utyatka@gmail.com>
2020-05-07 23:26:22 +0200

    wip

--- local/templates/manom/assets/js/ui-accord.js
+++ local/templates/manom/assets/js/ui-accord.js
@@ -1,24 +1,24 @@
-// 'use strict';
+'use strict';
 
-// $(function () {
-//   if (window.isMobile()) {
-//     $("#accord-mobile").accordion({
-//       heightStyle: "content",
-//       collapsible: true
-//     });
-//   }
+$(function () {
+  if (window.isMobile()) {
+    $("#accord-mobile").accordion({
+      heightStyle: "content",
+      collapsible: true
+    });
+  }
 
-//   // function scrollTo(element) {
-//   //   jQuery('html, body').animate({
-//   //     scrollTop: jQuery(element).offset().top
-//   //   }, 500);
-//   // }
+  // function scrollTo(element) {
+  //   jQuery('html, body').animate({
+  //     scrollTop: jQuery(element).offset().top
+  //   }, 500);
+  // }
 
-//   // jQuery('.accordion').on('accordionactivate', function (event, ui) {
-//   //   scrollTo(jQuery(event.target).find('.ui-accordion-header-active'))
-//   // });
+  // jQuery('.accordion').on('accordionactivate', function (event, ui) {
+  //   scrollTo(jQuery(event.target).find('.ui-accordion-header-active'))
+  // });
 
-// });
+});
 
 
 
