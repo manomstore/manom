66ca01e9bd4270a8f07e322264edb2ce29e15244
Ksenia Balashova <utyatka@gmail.com>
2020-05-11 00:14:29 +0200

    wip

--- local/templates/manom/assets/js/anchor.js
+++ local/templates/manom/assets/js/anchor.js
@@ -1,3 +1,10 @@
-// $('body').animate({
-//   scrollTop: $($(this).attr('href')).offset().top - 165
-// }, 1400);
+$(function () {
+
+
+  headerHeight = $(".header__wrapper--fix").height() + 5;
+  $('html,body').animate({
+    scrollTop: target.offset().top - headerHeight
+  }, 500);
+});
+
+var scroll = new SmoothScroll('a[href*="#"]');
--- local/templates/manom/assets/js/ui-accord.js
+++ local/templates/manom/assets/js/ui-accord.js
@@ -18,9 +18,6 @@ $(function () {
   $(".product-questions").click(function () {
     $("#tab4").prop('checked', true);
     $("#tab2").prop('checked', false);
-    $('body').animate({
-      scrollTop: $($(this).attr('href')).offset().top - 165
-    }, 1400);
   });
 
 });
