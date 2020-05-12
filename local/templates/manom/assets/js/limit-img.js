82dfbac4c5e1e43b26247387017757d6f496e49e
Ksenia Balashova <utyatka@gmail.com>
2020-05-12 17:48:08 +0200

    wip

--- local/templates/manom/assets/js/limit-img.js
+++ local/templates/manom/assets/js/limit-img.js
@@ -1,6 +1,6 @@
 $(function () {
-  var imglist = $(".product-photo__left").length
-  if (imglist > 1) {
+  var imglist = $(".product-photo__left").length;
+  if (imglist < 2) {
     $(".product-photo__left img").addClass(visually - hidden)
   }
 });
