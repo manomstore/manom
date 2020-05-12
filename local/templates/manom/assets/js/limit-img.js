cea8699fa39c082ef763968bf2a8c89998c534ee
Ksenia Balashova <utyatka@gmail.com>
2020-05-12 18:10:20 +0200

    wip

--- local/templates/manom/assets/js/limit-img.js
+++ local/templates/manom/assets/js/limit-img.js
@@ -1,5 +1,5 @@
 $(function () {
-  var imglist = $(".product-photo__left").length;
+  var imglist = $(".product-photo__left img").length;
   if (imglist < 2) {
     $(".product-photo__left").addClass("visually-hidden");
   }
