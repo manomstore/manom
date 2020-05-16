1674d5df905bfaee566d42c7c90403df9017bd34
Ksenia Balashova <utyatka@gmail.com>
2020-05-16 13:57:34 +0200

    recommend block

--- local/templates/manom/components/bitrix/catalog.section/recom/template.php
+++ local/templates/manom/components/bitrix/catalog.section/recom/template.php
@@ -35,6 +35,13 @@ $this->setFrameMode(true);
                                     Товар дня
                                 <?php endif; ?>
                             </p>
+                            <div class="p-nav-top">
+                                <label>
+                                    <input class="p-nav-top__checkbox" type="checkbox" <?=checkProdInFavoriteAndCompareList($arPrice['PRODUCT_ID'], 'UF_FAVORITE_ID') ? 'checked' : '';?>>
+                                    <div class="p-nav-top__favorite addToFavoriteList <?=!checkProdInFavoriteAndCompareList($arPrice['PRODUCT_ID'], 'UF_FAVORITE_ID') ? 'notActive' : '';?>" data-id='<?=$arPrice['PRODUCT_ID']?>' title="в избранное"></div>
+                                </label>
+                                <div class="p-nav-top__list addToCompareList <?=!checkProdInFavoriteAndCompareList($arPrice['PRODUCT_ID'], 'UF_COMPARE_ID') ? 'notActive' : 'alt-img';?>" data-id='<?=$arPrice['PRODUCT_ID']?>'></div>
+                            </div>
                             <!--  -->
                             <div class="p-nav-middle">
 
