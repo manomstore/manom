ffa8eb96ac3a796767dfc03728bbe35f6067f975
Pavel <pavel@hozberg.com>
2020-08-14 15:27:24 +0300

    Убрать раздел свойств "Другие"

--- local/templates/manom/components/redsign/grupper.list/catalog_element/template.php
+++ local/templates/manom/components/redsign/grupper.list/catalog_element/template.php
@@ -35,36 +35,3 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
         </div>
     <?php endif; ?>
 <?php endforeach; ?>
-
-<?php if (is_array($arResult['NOT_GROUPED_ITEMS']) && count($arResult['NOT_GROUPED_ITEMS']) > 0): ?>
-    <div class="redesign-characteristics col-12">
-        <strong class="name-properties">Другие:</strong>
-        <span></span>
-        <?php
-        $count = 0;
-        $secondCol = false;
-        ?>
-        <?php foreach ($arResult['NOT_GROUPED_ITEMS'] as $i => $prop): ?>
-            <?php
-            if ($prop['CODE'] === 'FILES') {
-                continue;
-            }
-            $count++;
-            ?>
-            <div class="row table-padding">
-                <span class="redesign-characteristics-name col-40"><?=$prop['NAME']?>:</span>
-                <span class="col-60">
-                    <? if (is_array($prop['DISPLAY_VALUE'])) { ?>
-                        <?=implode('; ', $prop['DISPLAY_VALUE'])?>
-                    <? } else {
-                        ?>
-                        <?=$prop['DISPLAY_VALUE']?>
-                    <? } ?>
-                </span>
-            </div>
-            <?php if ($secondCol === false && (count($arResult['DISPLAY_PROPERTIES']) / 2) <= $count) {
-                $secondCol = true;
-            } ?>
-        <?php endforeach; ?>
-    </div>
-<?php endif; ?>
