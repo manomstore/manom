6462d1bcae5936ffd0f9a517652f989c2a37810c
Ksenia Balashova <utyatka@gmail.com>
2020-09-02 03:35:04 +0200

    img

--- empty/index.php
+++ empty/index.php
@@ -6,19 +6,19 @@ $APPLICATION->SetTitle("нет в наличии");
     <div class="container">
         <div class="empty">
             <div class="empty__block empty__block--brand">
-              <img src="./local/templates/manom/assets/img/honor.svg" >
+              <img src="./../local/templates/manom/assets/img/honor.svg" >
               <p>
                 Здесь пока пусто. Посмотрите другие <a href="#">смартфоны</a>
               </p>
             </div>
-            <div class="empty__block empty__block--goods">
+            <!-- <div class="empty__block empty__block--goods">
               <h2>
                 Пылесосы
               </h2>
               <p>
                 Здесь пока пусто. Посмотрите другие товары в разделе <a href="#">техника для дома</a>
               </p>
-            </div>
+            </div> -->
         </div>
     </div>
 </div>
