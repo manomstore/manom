01ccf9040a94938a7c5b7b44432e5e553e54451c
Ksenia Balashova <utyatka@gmail.com>
2020-09-02 03:31:21 +0200

    add page

---
+++ empty/index.php
@@ -0,0 +1,20 @@
+<div class="content">
+    <div class="container">
+        <div class="empty">
+            <div class="empty__block empty__block--brand">
+              <img src="./local/templates/manom/assets/img/honor.svg" >
+              <p>
+                Здесь пока пусто. Посмотрите другие <a href="#">смартфоны</a>
+              </p>
+            </div>
+            <div class="empty__block empty__block--goods">
+              <h2>
+                Пылесосы
+              </h2>
+              <p>
+                Здесь пока пусто. Посмотрите другие товары в разделе <a href="#">техника для дома</a>
+              </p>
+            </div>
+        </div>
+    </div>
+</div>
