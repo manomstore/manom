9541e61670fd098448152cf7ea5feb43296b6978
Pavel <pavel@hozberg.com>
2020-09-09 15:28:01 +0300

    MAN-96 Синхронизация отмены с МС

--- local/modules/manom.moysklad/include.php
+++ local/modules/manom.moysklad/include.php
@@ -6,6 +6,8 @@ Loader::registerAutoLoadClasses(
     'manom.moysklad',
     array(
         'eventHandlers' => 'classes/general/eventHandlers.php',
+        'Manom\Moysklad\Moysklad\CustomerOrder' => 'lib/moysklad/customerOrder.php',
+        'Manom\Moysklad\Agent' => 'lib/Agent.php',
     )
 );
 
