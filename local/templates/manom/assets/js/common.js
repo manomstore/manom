7131dbaad25d119844823f11891fefddf5b44d8e
Ksenia Balashova <utyatka@gmail.com>
2020-10-15 22:20:42 +0200

    test style

--- local/templates/manom/assets/js/common.js
+++ local/templates/manom/assets/js/common.js
@@ -541,3 +541,20 @@ $(function () {
     }
   });
 });
+
+$( function() {
+  $( document ).tooltip({
+    position: {
+      my: "center bottom-20",
+      at: "center top",
+      using: function( position, feedback ) {
+        $( this ).css( position );
+        $( "<div>" )
+          .addClass( "catalog-tooltip" )
+          .addClass( feedback.vertical )
+          .addClass( feedback.horizontal )
+          .appendTo( this );
+      }
+    }
+  });
+} );
