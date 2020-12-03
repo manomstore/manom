b06bc3b48ff53adb6cd2a8ecaf4f12a37ff872c8
Ksenia Balashova <utyatka@gmail.com>
2020-12-03 11:29:01 +0100

    Revert "hide scrollbar"
    This reverts commit f15bc016383df0fff5f71ac6b06b48aff57e039c.

--- local/templates/manom/components/bitrix/catalog/catalog/bitrix/catalog.section.list/.default/template.php
+++ local/templates/manom/components/bitrix/catalog/catalog/bitrix/catalog.section.list/.default/template.php
@@ -7,22 +7,20 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 $this->setFrameMode(true);
 
 ?>
-<div  class="catalog-filter__category-cover">
-    <ul>
-        <?php if (!empty($arResult['SECTIONS'])): ?>
-            <? if (!empty($arResult["SECTION"]["NAME"])): ?>
-                <h3><?= $arResult["SECTION"]["NAME"] ?></h3>
-            <? endif; ?>
-            <li class="catalog-filter__category">
-
-                <? foreach ($arResult["SECTIONS"] as $section): ?>
-                    <p>
-                        <a class="catalog-filter__category-link" href="<?= $section["SECTION_PAGE_URL"] ?>">
-                            <?= $section["NAME"] ?>
-                        </a>
-                    </p>
-                <? endforeach; ?>
-            </li>
+<ul class="catalog-filter__category-cover">
+    <?php if (!empty($arResult['SECTIONS'])): ?>
+        <? if (!empty($arResult["SECTION"]["NAME"])): ?>
+            <h3><?= $arResult["SECTION"]["NAME"] ?></h3>
         <? endif; ?>
-    </ul>
-</div>
+        <li class="catalog-filter__category">
+
+            <? foreach ($arResult["SECTIONS"] as $section): ?>
+                <p>
+                    <a class="catalog-filter__category-link" href="<?= $section["SECTION_PAGE_URL"] ?>">
+                        <?= $section["NAME"] ?>
+                    </a>
+                </p>
+            <? endforeach; ?>
+        </li>
+    <? endif; ?>
+</ul>
