04d49ee7830db63b74bd8cd9fb72c909a799062e
Ksenia Balashova <utyatka@gmail.com>
2021-03-04 14:54:02 +0100

    add fav

--- local/templates/manom/header.php
+++ local/templates/manom/header.php
@@ -46,10 +46,13 @@
         <meta name="author" content="">
 
         <!-- Favicons -->
-        <link rel="shortcut icon" href="favicon.png">
-        <link rel="icon" href="<?=SITE_TEMPLATE_PATH?>/assets/img/favicon/favicon.ico">
-        <link rel="apple-touch-icon" sizes="180x180" href="<?=SITE_TEMPLATE_PATH?>/assets/img/favicon/apple-touch-icon-180x180.png">
-
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3d8e65">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
         <meta property="og:image" content="<?=SITE_TEMPLATE_PATH?>/assets/img/banner.jpg">
 
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Open+Sans:400,600,700&amp;subset=cyrillic-ext" rel="stylesheet">
