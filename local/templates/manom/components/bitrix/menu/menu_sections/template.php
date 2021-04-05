282906083ab192e7b916bd06826581ad83a087d9
Ksenia Balashova <utyatka@gmail.com>
2021-04-06 00:07:58 +0200

    t

--- local/templates/manom/components/bitrix/menu/menu_sections/template.php
+++ local/templates/manom/components/bitrix/menu/menu_sections/template.php
@@ -90,7 +90,7 @@ $this->setFrameMode(true); ?>
                                                                href="<?= $submenuItem["url"] ?>">
                                                                 <?= $submenuItem["name"] ?>
                                                             </a>
-                                                            <img style="width: auto; height: 35px; object-fit: contain;"
+                                                            <img style="width: auto; height: 35px; object-fit: contain; max-width: 50px"
                                                                  src="<?= $submenuItem["logo"] ?>"
                                                                  alt="<?= $submenuItem["name"] ?>">
                                                         </li>
